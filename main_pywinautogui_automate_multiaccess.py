0b56b95020360f734092e3d2c609c8e7a5769a69
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2020-09-13 09:05:39 +0200

    Detecting serial port error; printing progress

--- main_pywinautogui_automate_multiaccess.py
+++ main_pywinautogui_automate_multiaccess.py
@@ -3,6 +3,12 @@ import time
 import getpass
 from pywinauto.application import Application
 import psutil
+from colors import color
+
+
+def err(s):
+    errstr = color("ERROR", bg="RED", fg="WHITE")
+    print(f"{errstr}: {s}", file=sys.stderr)
 
 
 def is_multiaccess_running():
@@ -12,7 +18,7 @@ def is_multiaccess_running():
 print("Checking if MultiAccess is running...", end="")
 if is_multiaccess_running():
     app = Application(backend="uia").connect(path="C:\\Program Files (x86)\\MultiAccess\\MultiAccess.exe")
-    app.MultiAccess.kill()
+    app.kill()
 
 if False:
     print(" ERROR!\n")
@@ -34,6 +40,7 @@ while not logged_in:
     username = input("Username: ")
     password = getpass.getpass("Password: ")
 
+    print("Trying to login to MultiAccess...", end="")
     app_win.NamnEdit.draw_outline()
     app_win.NamnEdit.set_text(username)
     app_win.LösenordEdit.draw_outline()
@@ -43,18 +50,41 @@ while not logged_in:
 
     logged_in = not app_win.window(title="Felaktigt namn eller lösenord!").exists(timeout=1)
     if not logged_in:
-        print("The login was incorrect. Try again...")
+        print(" ERROR\n")
+        err("The login was incorrect. Try again...")
         app_win.OK.click()
+    else:
+        print(" OK!")
 
 app_win = app.MultiAccess
 app_win.restore()
+
+print("Starting synchronization")
 send_changes_button = app_win.Toolbar.Button9
 send_changes_button.draw_outline()
 send_changes_button.click()
 
-app_win.Kommunikation.wait("exists")
+print("Checking that synchronization starts")
+app_win.window(title_re="Kommunikation").wait("exists")
 
-app_win["Uppdatering fullständig."].wait("exists", timeout=70)
+print("Waiting for synchronization to finish")
+update_ok = None
+start_time = time.time()
+while True:
+    if app_win["Serieport ej tillgänglig."].exists():
+        update_ok = False
+        err("Connect serial port and try again.")
+        app.kill()
+        sys.exit(-1)
+    if app_win["Uppdatering fullständig."].exists():
+        update_ok = True
+        break
+    if time.time() - start_time > 15:
+        err("Timeout...")
+        app.kill()
+        sys.exit(-1)
+
+print("Synchronization done. Closing.")
 ok_button = app_win.OK
 ok_button.draw_outline()
 ok_button.click()
